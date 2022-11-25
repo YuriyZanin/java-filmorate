4a0a09f72b3b06e75d8a4664585043ca927f8633
YuriyZanin <y.v.zanin@yandex.ru>
2022-11-25 16:56:56 +0400

    refactor: возвращать код 400 при ошибках валидации и 404 при попытке обновить несуществующий объект

--- src/main/java/ru/yandex/practicum/filmorate/util/exeption/AlreadyExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/exeption/AlreadyExistException.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.util.exeption;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class AlreadyExistException extends RuntimeException {
     public AlreadyExistException(String s) {
         super(s);
